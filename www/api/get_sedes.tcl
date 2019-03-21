6478d901717403ac1c729e714653a844110a82b5
sebaszx <juandediostec@gmail.com>
2019-03-21 00:52:28 -0500

    un nuevo tcl

---
+++ www/api/get_sedes.tcl
@@ -0,0 +1,19 @@
+ad_page_contract {
+	inclusiones API
+	@author juan_jop96@hotmail.com sebaspf1997@gmail.com
+	@creation-date Mar 2019
+} {
+	
+}
+
+
+set answer [incl::get_sedes]
+
+if {$answer eq -1} {
+    set answer "\{\"title\":\"Bad Request\",\"body\":\"Problem to get admin page.\"\}"
+    ns_return 400 "application/json; charset=utf-8" $answer
+    ad_script_abort
+}
+
+ns_return 200 "application/json; charset=utf-8" $answer
+ad_script_abort
