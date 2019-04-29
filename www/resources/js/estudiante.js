1cf2ca04f6474584520385f658b3ff71ff4eb0f3
SpiritHunt3r <juandediostec@gmail.com>
2019-04-29 16:48:04 -0500

    Mejora interfaz de inclusion

--- www/resources/js/estudiante.js
+++ www/resources/js/estudiante.js
@@ -11,12 +11,6 @@ app.controller('estudianteController', function($scope, $http){
 
     $(document).ready(function() {
     $('#example').DataTable( {
-        "initComplete": function () {
-            var api = this.api();
-            api.$('td').click( function () {
-                api.search( this.innerHTML ).draw();
-            } );
-        },
         "language": {
             "url": "//cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json"
                 },
