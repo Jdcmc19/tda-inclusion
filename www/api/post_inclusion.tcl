271823ce18b52253c4e851590f03a52f531693dc
SpiritHunt3r <juandediostec@gmail.com>
2019-05-04 13:20:46 -0500

    new inclusion

--- www/api/post_inclusion.tcl
+++ www/api/post_inclusion.tcl
@@ -8,6 +8,10 @@ ad_page_contract {
 	{sede_id ""}
 	{escuela_id ""}
 	{curso_id ""}
+	{modalidad_nombre ""}
+	{sede_nombre ""}
+	{escuela_nombre ""}
+	{curso_nombre ""}
 	{grupo_id ""}
 	{comentario_asunto ""}
 	{comentario_mensaje ""}
@@ -15,7 +19,7 @@ ad_page_contract {
 }
 
 
-set answer [incl::insert_inclusion -modalidad_id $modalidad_id -periodo_id $periodo_id -sede_id $sede_id -escuela_id $escuela_id -curso_id $curso_id -grupo_id $grupo_id -comentario_asunto $comentario_asunto -comentario_mensaje $comentario_mensaje]
+set answer [incl::insert_inclusion -modalidad_id $modalidad_id -periodo_id $periodo_id -sede_id $sede_id -escuela_id $escuela_id -curso_id $curso_id -modalidad_nombre $modalidad_nombre -sede_nombre $sede_nombre -escuela_nombre $escuela_nombre -curso_nombre $curso_nombre -grupo_id $grupo_id -comentario_asunto $comentario_asunto -comentario_mensaje $comentario_mensaje]
 
 if {$answer eq -1} {
     set answer "\{\"title\":\"Bad Request\",\"body\":\"Problem to get admin page.\"\}"
--- www/resources/js/inclusion.js
+++ www/resources/js/inclusion.js
@@ -59,7 +59,7 @@ $scope.enviarInclusion = function(){
                 str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
                 return str.join("&");
             },
-            data : { modalidad_id: $scope.inclusion.modalidades.nombre_modalidad, periodo_id: $scope.inclusion.periodos.id_periodo ,sede_id: $scope.inclusion.sedeSeleccionada.nombre_sede, escuela_id: $scope.inclusion.escuelaSeleccionada.nombre_escuela, curso_id: $scope.inclusion.cursoSeleccionada.nombre_curso, grupo_id: $scope.inclusion.grupoSeleccionada.id_grupo, comentario_asunto: $scope.inclusion.asusnto, comentario_mensaje: $scope.inclusion.comentario }
+            data : { modalidad_id: $scope.inclusion.modalidades.id_modalidad, periodo_id: $scope.inclusion.periodos.id_periodo ,sede_id: $scope.inclusion.sedeSeleccionada.id_sede, escuela_id: $scope.inclusion.escuelaSeleccionada.id_escuela, curso_id: $scope.inclusion.cursoSeleccionada.id_curso, modalidad_nombre: $scope.inclusion.modalidades.nombre_modalidad,sede_nombre: $scope.inclusion.sedeSeleccionada.nombre_sede, escuela_nombre: $scope.inclusion.escuelaSeleccionada.nombre_escuela, curso_nombre: $scope.inclusion.cursoSeleccionada.nombre_curso, grupo_id: $scope.inclusion.grupoSeleccionada.id_grupo, comentario_asunto: $scope.inclusion.asusnto, comentario_mensaje: $scope.inclusion.comentario }
         }).then(function(response){
             console.dir(response);
 
